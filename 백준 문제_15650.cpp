6b1b8f5bd0a44b84de1d3552beb4525fb315b190
cyp0922 <69453493+cyp0922@users.noreply.github.com>
2020-12-29 17:07:19 +0900

    Create 백준 문제_15650.cpp

---
+++ 백준 문제_15650.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+int N,M;
+int arr[9];
+int visited[9]; 
+
+void back(int x){
+	
+	if(x == M){
+		for(int i=1; i<= M; i++){
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	
+	for(int i=arr[x]+1; i<= N; i++){
+		if(!visited[i]){
+			visited[i] = true;
+			arr[x+1] = i; 
+			back(x+1);
+			visited[i] = false;
+		}
+	}
+	
+	
+}
+
+int main(){
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+
+	cin >> N >> M;
+	
+	back(0);
+	
+
+}
+
