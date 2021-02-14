61fa505d2c0ef75bb7a529a39091d167f2774403
cyp0922 <69453493+cyp0922@users.noreply.github.com>
2021-02-15 05:19:01 +0900

    Update 나머지의 합_10986번.cpp

--- 나머지의 합_10986번.cpp
+++ 나머지의 합_10986번.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 
 int N, M;
-int arr[1000001];
 int store[1001];
 
 int main() {
@@ -12,21 +11,19 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
-	cin >> N >> M;
-	for (int i = 1; i <= N; i++) {
-		cin >> arr[i];
-	}
-
 	ll ans = 0;
 	ll sum = 0;
+	int input;
 
+	cin >> N >> M;
 	for (int i = 1; i <= N; i++) {
-		sum += arr[i];
+		cin >> input;
+		sum += input;
 		if (sum % M == 0) {
 			ans++;
 			ans += store[sum % M];
 		}
-		else if(store[sum % M]){
+		else if (store[sum % M]) {
 			ans += store[sum % M];
 		}
 		store[sum % M]++;
